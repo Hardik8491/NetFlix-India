b0103cc31ada36d37a6f355022db4de18ed79d9b
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-08-31 08:04:15 +0530

    first commit

---
+++ netflix-api/index.js
@@ -0,0 +1,31 @@
+const express =require("express");
+const userRoutes = require("./routes/UserRoutes");
+const cors =require("cors");
+const mongoose = require("mongoose");
+const dotenv =require('dotenv')
+var gi = require(`gitignore`);
+const app=express();
+app.use(express());
+app.use(cors());
+dotenv.config();
+
+
+ /* ------>MongoDb Connaction <------ */
+ mongoose
+ .connect(process.env.MONGODB_URL,
+ {  useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+ })
+ .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+  app.use("/api/user", userRoutes);
+
+ const PORT=3000;
+app.listen(PORT,()=>{
+    console.log("server started on Port 3000")
+})
